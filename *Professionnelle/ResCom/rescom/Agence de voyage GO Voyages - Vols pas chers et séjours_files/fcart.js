5127d497d9df247cd12ead9e5acd5bfbed642bac
aghiles <githubfortyuds@gmail.com>
2019-05-06 17:59:54 +0200

    commit

---
+++ *Professionnelle/ResCom/rescom/Agence de voyage GO Voyages - Vols pas chers et séjours_files/fcart.js
@@ -0,0 +1,43 @@
+(function () {
+        var s = document.createElement('script');
+        var p = "var i = document.createElement('img');\
+    i.setAttribute('alt', '');\
+    i.setAttribute('height', '1');\
+    i.setAttribute('width', '1');\
+    i.setAttribute('style', 'display: none;');\
+    i.setAttribute('src', 'https://tapestry.tapad.com/tapestry/1?ta_partner_did=S6xYRQeX5l6lYH9VXYK8yLfwb4mXiAxqN9Fetmx8537JGn_bVQJUUbaByWZU_G0n&ta_partner_id=996&ta_redirect=https://pixel.sojern.com/tapidSync?exchangeProfileId=${IDS:key}');\
+    document.body.appendChild(i);\
+";
+
+        s.type = 'text/javascript';
+        s.async = true;
+        if (s.textContent) {
+            s.textContent = p;
+        } else {
+            s.text = p;
+        }
+        var x = document.getElementsByTagName('script')[0];
+        x.parentNode.insertBefore(s, x);
+    })();
+    (function () {
+        var s = document.createElement('script');
+        var p = "var i = document.createElement('img');\
+    i.setAttribute('alt', '');\
+    i.setAttribute('height', '1');\
+    i.setAttribute('width', '1');\
+    i.setAttribute('style', 'display: none;');\
+    i.setAttribute('src', 'https://ib.adnxs.com/getuid?https://pixel.sojern.com/idSync/apn?sjrn_cid=XCEHmltrDHTxzHUvC89orunsEeURy0u4D8pSs58b9nMmmf-I9wJxcwDv6z-jDqPi&adnxs_uid=$UID');\
+    document.body.appendChild(i);\
+";
+
+        s.type = 'text/javascript';
+        s.async = true;
+        if (s.textContent) {
+            s.textContent = p;
+        } else {
+            s.text = p;
+        }
+        var x = document.getElementsByTagName('script')[0];
+        x.parentNode.insertBefore(s, x);
+    })();
+    
